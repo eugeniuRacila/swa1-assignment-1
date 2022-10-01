9b7c077c71462d3e1111633a33e2a1e7290d3b8a
IgorLopLego <102794680+IgorLopLego@users.noreply.github.com>
2022-10-01 11:10:39 +0200

    I have made an example of concatenation. Just use it and put in the for-loop to get the array of models

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ import "./style.css";
 import { setupCounter } from "./counter.js";
 import { fetchWeatherData } from "./src/repositories/measurementsRepository";
 import { CityData } from "./src/models/temp";
+import {fetchForecastData} from "./src/repositories/forecastRepository.js";
 
 document.querySelector("#app").innerHTML = `
   <div>
@@ -21,14 +22,63 @@ document.querySelector("#app").innerHTML = `
 setupCounter(document.querySelector("#counter"));
 
 const aarhusMeasurementsData = await fetchWeatherData("Aarhus");
-const aarhussData = CityData("Aarhus", aarhusMeasurementsData);
+//const aarhussData = CityData("Aarhus", aarhusMeasurementsData);
 
-const copenhagenMeasurementsData = await fetchWeatherData("Copenhagen");
-const copenhagenData = CityData("Copenhagen", copenhagenMeasurementsData);
 
-const horsensMeasurementsData = await fetchWeatherData("Horsens");
-const horsensData = CityData("Horsens", horsensMeasurementsData);
+//then you cal use the for loop to do the same for every element you are getting the data from
+let aarhusDataUnit = aarhusMeasurementsData[0];
+let IWeather = {type: aarhusDataUnit.type, unit: aarhusDataUnit.unit, time: aarhusDataUnit.time, place: aarhusDataUnit.place};
+let WeatherCompoundData;
+if(IWeather.type === "wind speed")
+{
+    WeatherCompoundData = Object.assign({},IWeather, {value: aarhusDataUnit.value,  direction : aarhusDataUnit.direction});
+}
+else if (IWeather.type === "precipitation")
+{
+    WeatherCompoundData = Object.assign({},IWeather, {value: aarhusDataUnit.value,  precipitation_type : aarhusDataUnit.precipitation_type});
+}
+else{
+    WeatherCompoundData = Object.assign({},IWeather, {value: aarhusDataUnit.value});
+}
 
-console.log(aarhussData);
-console.log(copenhagenData);
-console.log(horsensData);
+console.log("Weather data: " + JSON.stringify(WeatherCompoundData, null, 2));
+
+
+//The same logic with the loop here
+
+const aarhusMeasurementsForecast = await fetchForecastData("Aarhus");
+//console.log("Aarhus forecast: " + JSON.stringify(aarhusMeasurementsForecast, null, 2));
+let aarhusDataUnitForecast = aarhusMeasurementsForecast[1];
+let IWeatherForecast = {type: aarhusDataUnitForecast.type, unit: aarhusDataUnitForecast.unit, time: aarhusDataUnitForecast.time, place: aarhusDataUnitForecast.place};
+let WeatherForecastData;
+
+if(IWeatherForecast.type === "precipitation")
+{
+    WeatherForecastData = Object.assign({}, IWeatherForecast, {from: aarhusDataUnitForecast.from, to: aarhusDataUnitForecast.to, precipitation_types: aarhusDataUnitForecast.precipitation_types});
+}
+else if (IWeatherForecast.type === "directions")
+{
+    WeatherForecastData = Object.assign({}, IWeatherForecast, {from: aarhusDataUnitForecast.from, to: aarhusDataUnitForecast.to, directions : aarhusDataUnitForecast.directions});
+}
+else{
+    WeatherForecastData = Object.assign({}, IWeatherForecast, {from: aarhusDataUnitForecast.from, to: aarhusDataUnitForecast.to});
+}
+
+ console.log("Weather forecast is: " + JSON.stringify(WeatherForecastData));
+
+
+
+
+// const copenhagenMeasurementsData = await fetchWeatherData("Copenhagen");
+// const copenhagenData = CityData("Copenhagen", copenhagenMeasurementsData);
+//
+// const horsensMeasurementsData = await fetchWeatherData("Horsens");
+
+
+
+
+// const horsensData = CityData("Horsens", horsensMeasurementsData);
+//
+// console.log(aarhussData);
+// console.log(copenhagenData);
+// console.log(horsensData);
--- src/repositories/forecastRepository.js
+++ src/repositories/forecastRepository.js
@@ -1,5 +1,5 @@
 export const fetchForecastData = async () => {
-  const response = await fetch("http://localhost:8080/forecast");
+  const response = await fetch("http://localhost:8080/forecast/Aarhus");
   const data = await response.json();
 
   return data;
