f28c318c5a0eb1649a1cb4d0edbd26c686dd329b
Eugeniu Racila <eugeniu.racila@gmail.com>
2022-10-02 13:43:31 +0200

    add static ui implementation

--- main.js
+++ main.js
@@ -4,21 +4,6 @@ import { fetchWeatherData } from "./src/repositories/measurementsRepository";
 import { CityData } from "./src/models/temp";
 import { fetchForecastData } from "./src/repositories/forecastRepository.js";
 
-document.querySelector("#app").innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`;
-
 setupCounter(document.querySelector("#counter"));
 
 const aarhusMeasurementsData = await fetchWeatherData("Aarhus");
